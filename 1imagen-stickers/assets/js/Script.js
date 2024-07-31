0af9eb76e7227f48bcf985c77f37bfb4b3844817
Doris Andrea Valverde Jara <andrea.avj03@gmail.com>
2024-07-31 00:41:31 -0400

    segunda tarea

--- 1imagen-stickers/assets/js/Script.js
+++ 1imagen-stickers/assets/js/Script.js
@@ -8,3 +8,13 @@ const manzana = ()=> {
     tabla.style.border = '2px solid red'
     }
 }
+// tarea numero dos
+
+const mostrar = ()=> {
+    const sumauno =parseFloat(document.querySelector('.stickeruno').value) || 0;
+    const sumados =parseFloat(document.querySelector('.stickerdos').value) || 0;
+    const sumatres =parseFloat(document.querySelector('.stickertres').value) || 0;
+
+    const suma = sumauno + sumados + sumatres;
+    
+}
