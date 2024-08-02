3ef889c2fb8d24c779d90d142383230359b4ab3e
Doris Andrea Valverde Jara <andrea.avj03@gmail.com>
2024-08-02 00:28:06 -0400

    termino tarea tres

---
+++ 1 imagen/assets/js/script.js
@@ -0,0 +1,10 @@
+// tarea numero uno
+const manzana = ()=> {
+    tabla = document.querySelector('.mi-imagen')
+    if(tabla.style.border === '2px solid red'){
+        tabla.style.border = 'none'
+    }
+    else{
+    tabla.style.border = '2px solid red'
+    }
+}
--- 2stickers/assets/js/Script.js
+++ 2stickers/assets/js/Script.js
@@ -1,13 +1,4 @@
-// tarea numero uno
-const manzana = ()=> {
-    tabla = document.querySelector('.mi-imagen')
-    if(tabla.style.border === '2px solid red'){
-        tabla.style.border = 'none'
-    }
-    else{
-    tabla.style.border = '2px solid red'
-    }
-}
+
 // tarea numero dos
 
 const verificar = ()=> {
